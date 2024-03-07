156c84c99425cba14c7f1d91feccfd7ceab5fe2d
Andree <daniel.arancibia40o0@gmail.com>
2024-03-07 10:22:44 -0400

    Arancibia Aguilar Daniel Andree metodos power y logarithm

--- Complex.java
+++ Complex.java
@@ -25,26 +25,28 @@ public class Complex {
 
     @Override
     public String toString() {
-        if(img>0){
+        if (img > 0) {
             return "(" + real + " + " + img + "i)";
-        }else{
+        } else {
             return "(" + real + " - " + Math.abs(img) + "i)";
         }
-        
+
     }
+
     /* Parte de Pablo Lopez Chavez */
     /* Division / Conjugate */
-    public Complex Division(Complex y){
-        double denominador =Math.pow(y.real,2)+Math.pow(y.img,2);
-        if(denominador==0){
+    public Complex Division(Complex y) {
+        double denominador = Math.pow(y.real, 2) + Math.pow(y.img, 2);
+        if (denominador == 0) {
             throw new Error("Division por cero");
         }
-        double newReal=(this.real*y.real+this.img*y.img)/denominador;
-        double newImg=(this.img*y.real-this.real*y.img)/denominador;
-        return new Complex(newReal,newImg);
+        double newReal = (this.real * y.real + this.img * y.img) / denominador;
+        double newImg = (this.img * y.real - this.real * y.img) / denominador;
+        return new Complex(newReal, newImg);
     }
-    public Complex Conjugate(Complex y){
-        return new Complex(y.real,-y.img);
+
+    public Complex Conjugate(Complex y) {
+        return new Complex(y.real, -y.img);
     }
     /* Parte de Bruno Monzon */
     /* Module / Square */
@@ -54,6 +56,21 @@ public class Complex {
 
     /* Parte de Daniel Arancibia */
     /* Power / Logarithm */
+    public Complex power(int n) {
+        double r = Math.pow(Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.img, 2)), n);
+        double theta = Math.atan2(this.img, this.real);
+        double newReal = r * Math.cos(n * theta);
+        double newImg = r * Math.sin(n * theta);
+        return new Complex(newReal, newImg);
+    }
+
+    public Complex logarithm() {
+        double r = Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.img, 2));
+        double theta = Math.atan2(this.img, this.real);
+        double newReal = Math.log(r);
+        double newImg = theta;
+        return new Complex(newReal, newImg);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -67,6 +84,8 @@ public class Complex {
         double real2 = scanner.nextDouble();
         System.out.println("Ingresa el segundo numero imaginario:");
         double imag2 = scanner.nextDouble();
+        System.out.println("Ingresa la potencia a elevar:");
+        Integer pot = scanner.nextInt();
 
         Complex num1 = new Complex(real1, imag1);
         Complex num2 = new Complex(real2, imag2);
@@ -76,17 +95,28 @@ public class Complex {
         Complex addition = num1.addition(num2);
         Complex subtraction = num1.subtraction(num2);
         Complex multiplication = num1.multiplication(num2);
-        Complex ConjugateNum1=num1.Conjugate(num1);
+        /* Parte de Pablo Lopez Chavez */
+        Complex ConjugateNum1 = num1.Conjugate(num1);
         Complex ConjugateNum2 = num2.Conjugate(num2);
         Complex Division = num1.Division(num2);
+        /* Parte de Daniel Arancibia */
+        Complex powerNum1 = num1.power(pot);
+        Complex logarithmNum1 = num1.logarithm();
+        Complex powerNum2 = num2.power(pot);
+        Complex logarithmNum2 = num2.logarithm();
 
         System.out.println("suma: " + addition.toString());
         System.out.println("resta: " + subtraction.toString());
         System.out.println("Multiplicacion: " + multiplication.toString());
+        /* Parte de Pablo Lopez Chavez */
         System.out.println("Conjugado del numero 1: " + ConjugateNum1.toString());
         System.out.println("Conjugado del numero 2: " + ConjugateNum2.toString());
         System.out.println("Division: " + Division.toString());
-
+        /* Parte de Daniel Arancibia */
+        System.out.println("Potencia Num1: " + powerNum1.toString());
+        System.out.println("Logaritmo Num1: " + logarithmNum1.toString());
+        System.out.println("Potencia Num2: " + powerNum2.toString());
+        System.out.println("Logaritmo Num2: " + logarithmNum2.toString());
         scanner.close();
     }
 }
